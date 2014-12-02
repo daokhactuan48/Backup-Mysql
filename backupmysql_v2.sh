fde0c6728ad4f08278846c7af3ebcce28c656b10
daokhactuan48 <khactuank55@yahoo.com>
2014-12-02 23:54:54 +0700

    Phiên bản 2
    Chú ý: Cài đặt ccrypt để sử dụng fle shell

--- backupmysql_v2.sh
+++ backupmysql_v2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Trước khi chạy file shell này ta phải cài đặt tools ccrypt với lệnh sau: # apt-get install ccrypt
+# Cai dat ccrypt truoc khi chay shell: # apt-get install ccrypt
 backup_dir="/var/lib/backups/mysql"
 filename="${backup_dir}/mysql-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
 # Dump the entire MySQL database
