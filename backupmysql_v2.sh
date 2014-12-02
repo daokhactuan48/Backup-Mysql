b247dafa32b0dbe2e7658c8159c11d9f721bbdf8
daokhactuan48 <khactuank55@yahoo.com>
2014-12-02 23:47:55 +0700

    Phiên bản 2
    Update thêm câu lệnh để mã hóa file  backup

---
+++ backupmysql_v2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+backup_dir="/var/lib/backups/mysql"
+filename="${backup_dir}/mysql-`hostname`-`eval date +%d%m%Y-%H%M%S`.sql.gz"
+# Dump the entire MySQL database
+mysqldump --opt -u root --all-databases | gzip > $filename
+# Ma hóa file với password là Admin123
+ccrypt $filename -KAdmin123
